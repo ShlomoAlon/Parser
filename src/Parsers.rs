adf9e9704743e2cd0028305025106c6d08ff3dd8
solomon-the-wise <99106363+solomon-the-wise@users.noreply.github.com>
2022-06-11 16:37:35 -0400

    used the new type pattern to drastically simplify code.

---
+++ src/Parsers.rs
@@ -0,0 +1,187 @@
+use anyhow::{anyhow, Result};
+use std::ops::Deref;
+use std::rc::Rc;
+pub struct OkP<'a, A> {
+    remaining_str: &'a str,
+    ast: A,
+}
+
+impl<'a, A> OkP<'a, A> {
+    fn new(remaining_str: &'a str, ast: A) -> OkP<'a, A> {
+        OkP {
+            remaining_str,
+            ast: ast,
+        }
+    }
+}
+pub type ParseResult<'a, A> = Result<OkP<'a, A>>;
+
+type ParserInner<A> = Rc<dyn for<'a> Fn(&'a str) -> ParseResult<'a, A>>;
+
+struct Parser<A: 'static>(ParserInner<A>);
+
+impl<A> Deref for Parser<A> {
+    type Target = ParserInner<A>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<A> Parser<A> {
+    fn new(p: impl for<'a> Fn(&'a str) -> ParseResult<'a, A> + 'static) -> Self {
+        Parser::<A> { 0: Rc::new(p) }
+    }
+
+    fn map_ast<B>(self, f: impl Fn(A) -> B + 'static) -> Parser<B> {
+        Parser::new(move |t: &str| match self(t) {
+            Ok(s) => ok_parse(s.remaining_str, f(s.ast)),
+            Err(_) => Err(anyhow!("didnt work")),
+        })
+    }
+    fn lift2<B, C>(self, p2: Parser<B>, f: impl Fn(A, B) -> C + 'static) -> Parser<C> {
+        Parser::new(move |t: &str| {
+            let res1 = self(t)?;
+            let res2 = p2(res1.remaining_str)?;
+            ok_parse(res2.remaining_str, f(res1.ast, res2.ast))
+        })
+    }
+    fn map<B>(self, f: impl Fn(ParseResult<A>) -> ParseResult<B> + 'static) -> Parser<B> {
+        Parser::new(move |t| f(self(t)))
+    }
+
+    fn many(self, min: usize, max: usize) -> Parser<Vec<A>> {
+        Parser::new(move |t: &str| {
+            let mut result = vec![];
+            let mut text = t;
+            loop {
+                match self(text) {
+                    Ok(i) => {
+                        result.push(i.ast);
+                        text = i.remaining_str;
+                    }
+                    Err(_) => break,
+                }
+                if result.len() == max {
+                    break;
+                }
+            }
+            if result.len() < min {
+                Err(anyhow!("no matcht"))
+            } else {
+                ok_parse(text, result)
+            }
+        })
+    }
+    fn many_one(self) -> Parser<Vec<A>> {
+        self.many(1, 0)
+    }
+    fn many_min(self, min: usize) -> Parser<Vec<A>> {
+        self.many(min, 0)
+    }
+    fn sequence(v: Vec<Self>) -> Parser<Vec<A>> {
+        Parser::new(move |t: &str| {
+            let mut text = t;
+            let mut result = vec![];
+            for i in &v {
+                let r = i(text)?;
+                result.push(r.ast);
+                text = r.remaining_str;
+            }
+            ok_parse(text, result)
+        })
+    }
+
+    fn choice(s: Vec<Self>) -> Self {
+        Parser::new(move |t: &str| {
+            let text = t;
+            let mut s = s.iter();
+            loop {
+                if let Ok(i) = s.next().ok_or(anyhow!("no match"))?(text) {
+                    return Ok(i);
+                }
+            }
+        })
+    }
+
+    fn option(self) -> Parser<Option<A>> {
+        Parser::new(move |t: &str| match self(t) {
+            Ok(i) => ok_parse(i.remaining_str, Some(i.ast)),
+            Err(_) => ok_parse(t, None),
+        })
+    }
+
+    fn bind<B>(self, f: impl Fn(A) -> Parser<B> + 'static) -> Parser<B> {
+        Parser::new(move |t| {
+            let x = self(t)?;
+            let p2 = f(x.ast);
+            p2(x.remaining_str)
+        })
+    }
+    fn fail(err: String) -> Self{
+        Parser::new(move |_| Err(anyhow!("didnt work")))
+    }
+}
+
+impl<A: Default> Parser<A> {
+    fn not(self) -> Parser<A> {
+        Parser::new(move |t: &str| match &self(t) {
+            Ok(_) => Err(anyhow!("it succeeded when we wanted failure")),
+            Err(_) => ok_parse(t, A::default()),
+        })
+    }
+}
+
+impl<A: Clone> Parser<A> {
+    fn or_default(self, default: A) -> Self {
+        Parser::new(move |t| Ok(self(t).unwrap_or(OkP::new(t, default.clone()))))
+    }
+    fn default(x: A) -> Parser<A> {
+        Parser::new(move |t| ok_parse(t, x.clone()))
+    }
+}
+impl Parser<String> {
+    fn literal<A: ToString>(l: A) -> Self {
+        let l = l.to_string();
+        Parser::new(move |t| {
+            if t.starts_with(&l) {
+                let (remaining_str, ast) = t.split_at(l.len());
+                ok_parse(remaining_str, ast.to_string())
+            } else {
+                Err(anyhow!("diddnt work"))
+            }
+        })
+    }
+}
+
+impl Parser<char> {
+    fn any() -> Self {
+        Parser::new(|t: &str| {
+            if t.len() == 0 {
+                Err(anyhow!("String is empty"))
+            } else {
+                ok_parse(&t[1..], t.chars().nth(0).unwrap())
+            }
+        })
+    }
+    fn char_predicate(f: impl Fn(char) -> bool + 'static) -> Self{
+        Parser::any().bind(move |t|{
+            if f(t){
+                Parser::default(t)
+            } else {
+                Parser::fail("didnt work".to_string())
+            }
+        })
+    }
+}
+
+impl<A> Parser<Vec<A>> {
+    fn join(self, parser2: Self) -> Self {
+        self.lift2(parser2, |mut x, mut y| {
+            x.append(&mut y);
+            x
+        })
+    }
+}
+pub fn ok_parse<A>(remaining_str: &str, ast: A) -> ParseResult<A> {
+    Ok(OkP::new(remaining_str, ast))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,280 +1,41 @@
-use std::iter::Flatten;
-use std::rc::Rc;
+mod Parsers;
 
-use anyhow::{anyhow, Result};
 
 fn main() {}
 
-type Ast<A> = A;
-type ParseResult<'a, A> = Result<(&'a str, Ast<A>)>;
-type Parser<A> = Rc<dyn for<'a> Fn(&'a str) -> ParseResult<'a, A>>;
-
-trait to_literal {
-    fn lit(&self) -> Parser<String>;
-}
-
-trait to_literal2 {
-    fn lit(self) -> Parser<String>;
-}
-impl to_literal for str {
-    fn lit(&self) -> Parser<String> {
-        literal(self.to_string())
-    }
-}
-
-impl to_literal2 for String {
-    fn lit(self) -> Parser<String> {
-        literal(self)
-    }
-}
-
-trait Parsers<A> {
-    fn many_one(self) -> Parser<Vec<A>>;
-    fn map_ast<B>(self, f: impl Fn(A) -> B + 'static) -> Parser<B>;
-    fn not(self) -> Parser<A>;
-    fn option(self) -> Parser<Option<A>>;
-    fn sequence(self, x: &mut Vec<Parser<A>>) -> Parser<Vec<A>>;
-    fn choice(self, x: &mut Vec<Parser<A>>) -> Parser<A>;
-    fn lift2<B: 'static, C>(self, p2: Parser<B>, f: impl Fn(A, B) -> C + 'static) -> Parser<C>;
-    fn or_default(self, x: A) -> Parser<A>;
-    fn bind<B>(self, f: impl Fn(A) -> Parser<B> + 'static) -> Parser<B>;
-}
-trait P<A>{
-    fn join( self, p2: Parser<A>) -> Self;
-}
-
-impl<A: 'static + Clone + Default> P<Vec<A>> for Parser<Vec<A>> {
-    fn join(self, p2: Parser<Vec<A>>) -> Self {
-        self.lift2(p2, |mut x, mut y| {
-            x.append(&mut y);
-            x
-        })
-    }
-}
-
-impl<A: 'static + Default + Clone> Parsers<A> for Parser<A> {
-    fn many_one<'a>(self) -> Parser<Vec<A>> {
-        many_one(self)
-    }
-    fn map_ast<B>(self, f: impl Fn(A) -> B + 'static) -> Parser<B> {
-        map_ast(self, f)
-    }
-    fn not(self) -> Parser<A> {
-        not(self)
-    }
-
-    fn option(self) -> Parser<Option<A>> {
-        option(self)
-    }
-
-    fn sequence(self, x: &mut Vec<Parser<A>>) -> Parser<Vec<A>> {
-        let mut v = vec![self];
-        v.append(x);
-        sequence(v)
-    }
-
-    fn choice(self, x: &mut Vec<Parser<A>>) -> Parser<A> {
-        let mut v = vec![self];
-        v.append(x);
-        choice(v)
-    }
-
-    fn lift2<B: 'static, C>(self, p2: Parser<B>, f: impl Fn(A, B) -> C + 'static) -> Parser<C> {
-        lift2(self, p2, f)
-    }
-
-    fn or_default(self, x: A) -> Parser<A> {
-        or_default(self, x)
-    }
-
-    fn bind<B>(self, f: impl Fn(A) -> Parser<B> + 'static) -> Parser<B> {
-        bind(self, f)
-    }
-}
-
-trait vecParser<A> {
-    fn choice(self) -> Parser<A>;
-    fn sequence(self) -> Parser<Vec<A>>;
-}
-
-impl<A: 'static> vecParser<A> for Vec<Parser<A>> {
-    fn choice(self) -> Parser<A> {
-        choice(self)
-    }
-
-    fn sequence(self) -> Parser<Vec<A>> {
-        sequence(self)
-    }
-}
-pub fn map<A: 'static, B>(
-    p: Parser<A>,
-    f: impl Fn(ParseResult<A>) -> ParseResult<B> + 'static,
-) -> Parser<B> {
-    Rc::new(move |t| f(p(t)))
-}
-pub fn literal(s: String) -> Parser<String> {
-    Rc::new(move |t: &str| {
-        if t.starts_with(&s) {
-            let t = t.split_at(s.len());
-            Ok((t.1, t.0.to_owned()))
-        } else {
-            Err(anyhow!("didnt work"))
-        }
-    })
-}
-fn char_predicate(f: impl Fn(char) -> bool + 'static) -> Parser<char> {
-    any().bind(move |t| {
-        if f(t) {
-            default(t)
-        } else {
-            fail(format!("unexpected char {}", t))
-        }
-    })
-}
-
-fn bind<A: 'static, B>(p1: Parser<A>, f: impl Fn(A) -> Parser<B> + 'static) -> Parser<B> {
-    Rc::new(move |t| {
-        let x = p1(t)?;
-        let p2 = f(x.1);
-        p2(x.0)
-    })
-}
-
-fn float(t: &str) -> ParseResult<f64> {
-    let plus_or_minus = vec!["+".lit(), "-".lit()]
-        .choice()
-        .or_default("+".to_string());
-
-    let digits = (0..10)
-        .map(|x| x.to_string().lit())
-        .collect::<Vec<Parser<String>>>()
-        .choice()
-        .many_one();
-
-    let integer = plus_or_minus.lift2(digits.clone(), |mut b, n| {
-        (b + n.join("").as_str()).to_string()
-    });
-    let end = "."
-        .lit()
-        .lift2(digits.clone(), |mut b, n| {
-            (b + n.join("").as_str()).to_string()
-        })
-        .or_default("".to_string());
-
-    vec![integer, end]
-        .sequence()
-        .map_ast(|x| x.join("").parse::<f64>().unwrap())(t)
-}
-fn sequence<A: 'static>(s: Vec<Parser<A>>) -> Parser<Vec<A>> {
-    Rc::new(move |t: &str| {
-        let mut text = t;
-        let mut result = vec![];
-        for i in &s {
-            let r = i(text)?;
-            result.push(r.1);
-            text = r.0;
-        }
-        Ok((text, result))
-    })
-}
-
-fn choice<A: 'static>(s: Vec<Parser<A>>) -> Parser<A> {
-    Rc::new(move |t: &str| {
-        let text = t;
-        let mut s = s.iter();
-        loop {
-            if let Ok(i) = s.next().ok_or(anyhow!("no match"))?(text) {
-                return Ok(i);
-            }
-        }
-    })
-}
-
-fn many_one<A: 'static>(s: Parser<A>) -> Parser<Vec<A>> {
-    many(s, 1, 0)
-}
-
-fn many_min<A: 'static>(s: Parser<A>, min: usize) -> Parser<Vec<A>> {
-    many(s, min, 0)
-}
-
-fn many<A: 'static>(s: Parser<A>, min: usize, max: usize) -> Parser<Vec<A>> {
-    Rc::new(move |t: &str| {
-        let mut result = vec![];
-        let mut text = t;
-        loop {
-            match s(text) {
-                Ok(i) => {
-                    result.push(i.1);
-                    text = i.0;
-                }
-                Err(_) => break,
-            }
-            if result.len() == max {
-                break;
-            }
-        }
-        if result.len() < min {
-            Err(anyhow!("no matcht"))
-        } else {
-            Ok((text, result))
-        }
-    })
-}
-
-fn not<A: 'static + Default>(s: Parser<A>) -> Parser<A> {
-    Rc::new(move |t: &str| match &s(t) {
-        Ok(_) => Err(anyhow!("it succeeded when we wanted failure")),
-        Err(_) => Ok((t, A::default())),
-    })
-}
-
-fn map_ast<A: 'static, B>(s: Parser<A>, f: impl Fn(A) -> B + 'static) -> Parser<B> {
-    Rc::new(move |t: &str| match s(t) {
-        Ok(s) => Ok((s.0, f(s.1))),
-        Err(_) => Err(anyhow!("didnt work")),
-    })
-}
-
-fn lift2<A: 'static, B: 'static, C>(
-    p1: Parser<A>,
-    p2: Parser<B>,
-    f: impl Fn(A, B) -> C + 'static,
-) -> Parser<C> {
-    Rc::new(move |t: &str| {
-        let res1 = p1(t)?;
-        let res2 = p2(res1.0)?;
-        Ok((res2.0, f(res1.1, res2.1)))
-    })
-}
-
-fn option<A: 'static>(p: Parser<A>) -> Parser<Option<A>> {
-    Rc::new(move |t: &str| match p(t) {
-        Ok(i) => Ok((i.0, Some(i.1))),
-        Err(_) => Ok((t, None)),
-    })
-}
-
-fn any() -> Parser<char> {
-    Rc::new(|t: &str| {
-        if t.len() == 0 {
-            Err(anyhow!("String is empty"))
-        } else {
-            Ok((&t[1..], t.chars().nth(0).unwrap()))
-        }
-    })
-}
-fn default<A: 'static + Clone>(x: A) -> Parser<A> {
-    Rc::new(move |t| Ok((t, x.clone())))
-}
-
-fn or_default<A: 'static + Clone>(p: Parser<A>, default: A) -> Parser<A> {
-    Rc::new(move |t| Ok(p(t).unwrap_or((t, default.clone()))))
-}
-fn fail<A>(err: String) -> Parser<A> {
-    Rc::new(move |_| Err(anyhow!("{}", err)))
-}
-fn to_vec<A: 'static + Default + Clone>(p: Parser<A>) -> Parser<Vec<A>> {
-    p.map_ast(|x| vec![x])
-}
-
+// fn char_predicate(f: impl Fn(char) -> bool + 'static) -> Parsers<char> {
+//     any().bind(move |t| {
+//         if f(t) {
+//             default(t)
+//         } else {
+//             fail(format!("unexpected char {}", t))
+//         }
+//     })
+// }
+//
+//
+// fn float(t: &str) -> ParseResult<f64> {
+//     let plus_or_minus = vec!["+".lit(), "-".lit()]
+//         .choice()
+//         .or_default("+".to_string());
+//
+//     let digits = (0..10)
+//         .map(|x| x.to_string().lit())
+//         .collect::<Vec<Parsers<String>>>()
+//         .choice()
+//         .many_one();
+//
+//     let integer = plus_or_minus.lift2(digits.clone(), |mut b, n| {
+//         (b + n.join("").as_str()).to_string()
+//     });
+//     let end = "."
+//         .lit()
+//         .lift2(digits.clone(), |mut b, n| {
+//             (b + n.join("").as_str()).to_string()
+//         })
+//         .or_default("".to_string());
+//
+//     vec![integer, end]
+//         .sequence()
+//         .map_ast(|x| x.join("").parse::<f64>().unwrap())(t)
+// }
